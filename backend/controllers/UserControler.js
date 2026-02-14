3dade992e75090db029099b8cab9906518707203
Chirag205444 <chiragshetty9204@gmail.com>
2026-02-14 22:21:31 +0530

    Initial commit

---
+++ backend/controllers/UserControler.js
@@ -0,0 +1,89 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+
+exports.signup = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    if (!username || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      username,
+      email,
+      password: hashedPassword
+    });
+
+    res.status(201).json({
+      message: "User registered successfully",
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email
+      }
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+
+
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign(
+      { id: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" }
+    );
+
+    // // Send token in cookie
+    // res.cookie("token", token, {
+    //   httpOnly: true,
+    //   secure: false, // true in production (https)
+    //   sameSite: "strict",
+    //   maxAge: 7 * 24 * 60 * 60 * 1000
+    // });
+
+    res.status(200).json({
+      token: token,
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email
+      }
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const cookieParser = require('cookie-parser');
+const express=require('express');
+const cors = require("cors");
+require("dotenv").config();
+const app=express();
+const {path} =require('path')
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser())
+app.use(express.urlencoded({extended : true}));
+app.use(express.static('public'));
+
+const UserRoute=require('./route/UserRoute')
+
+app.use("/user",UserRoute);
+
+
+const PORT=process.env.PORT || 3000;
+app.listen(PORT,()=>{
+    console.log(`${PORT}`);
+});
